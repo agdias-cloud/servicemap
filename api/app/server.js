609dc5b51a33a4522057142df8250ecef33e5f06
agdias-cloud <143414600+agdias-cloud@users.noreply.github.com>
2024-07-08 17:51:58 -0300

    Update server.js

--- api/app/server.js
+++ api/app/server.js
@@ -16,7 +16,7 @@ app.use('/api/v1/recursos/nome', recursos);
 app.use('/api/v1/recursos/id', recursos);
 
 
-app.listen(port, '0.0.0.0', () => console.log(`Now running on port ${port}`))
+app.listen(port,  () => console.log(`Now running on port ${port}`))
 
 
 
