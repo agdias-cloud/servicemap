e71816b5e894e2a36ddad5de15a740649c8c877c
ansible <ansible@docker-cloud.kube.local>
2024-07-08 16:31:36 -0300

    Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM node:22.4.0-slim
 RUN mkdir /app
 COPY api/app/ /app/
-ENTRYPOINT["NODE_DEV='prod'","node","api/app/server"]
+WORKDIR /app
+ENV NODE_ENV=${NODE_ENV}
+RUN npm install
+ENTRYPOINT ["node","/app/server.js"]
--- api/app/server.js
+++ api/app/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => console.log(`Now listening on port ${port}`))
 
 
