8a81ef6df65553be0f2dc2764c25fe750451aa04
agdias-cloud <143414600+agdias-cloud@users.noreply.github.com>
2024-07-08 17:01:59 -0300

    Update server.js

--- api/app/server.js
+++ api/app/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 3000;
+app.use(express.json());
 app.listen(port, () => console.log(`Now listening on port ${port}`))
 
 
